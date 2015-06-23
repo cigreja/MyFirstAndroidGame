5fa7cd90295cb381addc0a049f12142831224e63
cigreja <cigreja@live.com>
2015-06-22 20:07:25 -0400

    Game Loop

---
+++ app/src/main/java/com/cigreja/myfirstandroidgame/GamePanel.java
@@ -0,0 +1,65 @@
+package com.cigreja.myfirstandroidgame;
+
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by cigreja on 6/22/15.
+ */
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
+
+    private MainThread thread;
+
+    public GamePanel(Context context){
+
+        super(context);
+
+        // add the callback to the surfaceholder to intercept events
+        getHolder().addCallback(this);
+
+        thread = new MainThread(getHolder(), this);
+
+        // make GamePanel focusable so it can handle events
+        setFocusable(true);
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+        // we can safely start the game loop
+        thread.setRunning(true);
+        thread.start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+        boolean retry = true;
+
+        while(retry){
+            try{
+                thread.setRunning(false);
+                thread.join(); // blocks the calling thread until thread terminates
+                retry = false;
+            }
+            catch (Exception e){e.printStackTrace();} // end catch
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        return super.onTouchEvent(event);
+    }
+
+    public void update(){
+
+    }
+}
